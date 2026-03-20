a3521a070a8ebaa093354151add31261c087e953
tharushaDilhara <tharusha363@gmail.com>
2026-03-20 08:22:32 +0530

    CORS of AuthController finished

--- src/main/java/com/example/backend/Controller/AuthController.java
+++ src/main/java/com/example/backend/Controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
